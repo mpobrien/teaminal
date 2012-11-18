bb634e969e527c8c1398b59ab03a3e33280b41fc
Mike O'Brien <mikeo@10gen.com>
2012-11-18 14:51:43 -0500

    auto build base64 versions of test files

--- build.sh
+++ build.sh
@@ -1,9 +1,27 @@
 HERE="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+while getopts d opt
+do
+   case "$opt" in
+      d) NODEBUG=true;;
+   esac
+done
+
+echo "$NODEBUG"
+
 for file in $HERE/lib/screen.js $HERE/lib/termstream.js $HERE/lib/browserscreen.js $HERE/lib/b64binary.js
 do
     justfile=$(basename $file)
-    sed '/\@debug/d' $file > $HERE/build/$justfile
+    if [ $NODEBUG ]
+
+        then sed '/\@debug/d' $file > $HERE/build/$justfile
+    fi
+done
+
+for file in $HERE/testing/testfiles/*
+do
+    justfile=$(basename $file)
+    node $HERE/b64converter.js --file=$file > $HERE/pyapp/static/testfiles/$justfile
 done
 
 browserify $HERE/build/screen.js $HERE/build/termstream.js $HERE/build/browserscreen.js $HERE/build/b64binary.js > $HERE/build/teaminal.js 
