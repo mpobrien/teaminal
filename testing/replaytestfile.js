f033a67331007bb7d1d590fbe158948d225db64b
Mike O'Brien <mikeo@10gen.com>
2012-12-11 15:31:38 -0500

    fix test file replayer

--- testing/replaytestfile.js
+++ testing/replaytestfile.js
@@ -9,23 +9,23 @@ var BasicStream = require('../lib/termstream').BasicStream
 var BasicScreen = require('../lib/screen').BasicScreen
 var strm = new BasicStream(); 
 var scrn = new BasicScreen(40, 80)
-if(argv.d){
+//if(argv.d){
     scrn.debugMode = true
     strm.setDebugMode(true, true)
-}
+//}
 strm.attach(scrn)
 
 var filename = process.argv[2]
 var filedata = fs.readFileSync(argv.file, 'utf-8')
+console.log("got", filedata.length, "byte file.")
 
 var benchRuns = argv.b || 1;
 
 if(argv.r){
     var start = +new Date()
     for(var j=0;j<benchRuns;j++){
-        for(var i=0;i<filedata.length;i++){
-            strm.feed(filedata[i].charCodeAt(0))
-        }
+        console.log("feeding.")
+        strm.feed(filedata)
     }
     var end = +new Date()
     if(benchRuns>1){
@@ -41,9 +41,7 @@ if(argv.r){
         var line = lines[i]
         if(line){
             var linestr = JSON.parse(line)
-            for(var j=0;j<linestr.length;j++){
-                strm.feed(linestr[j])
-            }
+            strm.feed(linestr)
         }
     }
 }
